e9f565fe1b4b5a00cc663547abd47e626711e85d
SY81517 <63453966+SY81517@users.noreply.github.com>
2021-05-24 23:23:04 +0900

    コミット

---
+++ StudyNunit/MSdocNunit/MSdocNunit/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MSdocNunit
+{
+    class Program
+    {
+        static void Main( string[] args )
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
