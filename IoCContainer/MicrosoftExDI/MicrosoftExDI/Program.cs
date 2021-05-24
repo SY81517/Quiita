6a8181ed6f2306571a5421045ea92aa72a01de90
SY81517 <63453966+SY81517@users.noreply.github.com>
2021-05-25 02:18:08 +0900

    a

--- IoCContainer/MicrosoftExDI/MicrosoftExDI/Program.cs
+++ IoCContainer/MicrosoftExDI/MicrosoftExDI/Program.cs
@@ -6,6 +6,12 @@ using System.Threading.Tasks;
 
 namespace MicrosoftExDI
 {
+    /// <summary>
+    /// a
+    /// </summary>
+    /// <remarks>
+    /// https://docs.microsoft.com/ja-jp/dotnet/core/extensions/dependency-injection
+    /// </remarks>
     class Program
     {
         static void Main( string[] args )
