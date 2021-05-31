9b1fdf74cf60fafb35b1c99031e8a09fbce6c3c6
SY81517 <63453966+SY81517@users.noreply.github.com>
2021-06-01 00:01:05 +0900

    Stringと文字列補間に関する調査

---
+++ StudyString/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace StudyString
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            var plusResult = PlusOperator("Yamamoto");
+            var formatResult= StringFormat("Yamamoto");
+            var  interpolated = Interpolated("Yamamoto");
+        }
+        /// <summary>
+        /// 演算子結合
+        /// </summary>
+        /// <param name="arg"></param>
+        /// <returns></returns>
+        public static string PlusOperator(string arg)
+        {
+            return "Hello" + arg + "World";
+        }
+        /// <summary>
+        /// StringFormat
+        /// </summary>
+        /// <param name="arg"></param>
+        /// <returns></returns>
+        public static string StringFormat(string arg)
+        {
+            return string.Format("Hello{0}World",arg);
+        }
+        /// <summary>
+        /// 文字列補間
+        /// </summary>
+        /// <param name="arg"></param>
+        /// <returns></returns>
+        public static string Interpolated(string arg)
+        {
+          return  $"Hello{arg}World";
+        }
+    }
+}
