78cd1198163fd2a403357a9b73179eb4106dba92
SY81517 <63453966+SY81517@users.noreply.github.com>
2021-06-04 19:59:28 +0900

    up

---
+++ TryHttpBenchMark/BenchMark/WebApiClient.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace BenchMark
+{
+    public class WebApiClient
+    {
+        private static HttpClient Client = new HttpClient();
+
+        public WebApiClient()
+        {
+            Client.BaseAddress = new Uri("http://localhost:19691/");
+        }
+
+        /// <summary>
+        /// GETメソッドをコールする
+        /// </summary>
+        /// <returns></returns>
+        public async Task GetAsync()
+        {
+            try
+            {
+                Console.WriteLine("Call GET api/values");
+                var resource = await Client.GetAsync("api/values");
+                resource.EnsureSuccessStatusCode();
+                var responseBody = await resource.Content.ReadAsStringAsync();
+                Console.WriteLine(responseBody);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            Console.WriteLine("End");
+        }
+
+
+    }
+}
