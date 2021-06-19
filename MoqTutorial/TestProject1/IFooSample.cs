78811bb60910df4b4ed2526150140aba46940a6c
SY81517 <63453966+SY81517@users.noreply.github.com>
2021-06-20 08:52:22 +0900

    Update

---
+++ MoqTutorial/TestProject1/IFooSample.cs
@@ -0,0 +1,42 @@
+﻿using System;
+namespace MoqTutorial
+{
+    /// <summary>
+    /// 評価対象クラスのインターフェース
+    /// 参考
+    /// https://github.com/Moq/moq4/wiki/Quickstart
+    /// </summary>
+    public interface IFooSample
+    { 
+        bool DoSomething(string value);
+    }
+    /// <summary>
+    /// 評価対象クラス
+    /// </summary>
+    public class FooSample : IFooSample
+    {
+        private readonly IFooSample _ifooSampleObject;
+        /// <summary>
+        /// コンストラクタ・インジェクション
+        /// </summary>
+        /// <param name="iObject"></param>
+        public FooSample(IFooSample iObject)
+        {
+            _ifooSampleObject = iObject;
+        }
+        public bool DoSomething(string value)
+        {
+            var result = _ifooSampleObject.DoSomething(value);
+            //結果に応じた処理を記載する
+            if (result)
+            {
+                Console.WriteLine("成功時の処理を書く");
+            }
+            else
+            {
+                Console.WriteLine("成功時の処理を書く");
+            }
+            return result;
+        }
+    }
+}
---
+++ MoqTutorial/TestProject1/IFooSampleTest.cs
@@ -0,0 +1,22 @@
+﻿using Moq;
+using MoqTutorial;
+using NUnit.Framework;
+namespace TestProject1
+{
+    [TestFixture]
+    public class IFooSampleTest
+    {
+        [Test]
+        public void TestSample_Methoods()
+        {
+            //Mockを作成(インターフェースを指定)
+            var mock = new Mock<IFooSample>();
+            //関数名と戻り値を指定
+            mock.Setup(foo => foo.DoSomething("ping")).Returns(true);
+            //評価対象クラスにMockを渡す
+            var fooObject = new FooSample(mock.Object);
+            //評価対象クラスで関数コール
+            Assert.IsTrue(fooObject.DoSomething("ping"));
+        }
+    }
+}
