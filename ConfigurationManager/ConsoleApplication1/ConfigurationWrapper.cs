edd6eea7fe831003b5c21efff1f6f0146ebeffc6
SY81517 <63453966+SY81517@users.noreply.github.com>
2021-06-30 23:55:35 +0900

    Update

---
+++ ConfigurationManager/ConsoleApplication1/ConfigurationWrapper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Configuration;
+namespace ConsoleApplication1
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <remarks>
+    /// https://one-sthead.hatenablog.com/entry/2019/05/09/011909
+    /// https://stackoverflow.com/questions/9486087/how-to-mock-configurationmanager-appsettings-with-moq
+    /// </remarks>
+    public class AppConfiguration : IConfiguration
+    {
+        public string User() => ConfigurationManager.AppSettings["hoge"];
+    }
+}
